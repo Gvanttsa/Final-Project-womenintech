3d416d2e200a9fe67a24a62ba7b20a3843ced103
Gvanttsa <101789811+Gvanttsa@users.noreply.github.com>
2022-06-02 12:56:54 +0400

    add header

---
+++ script.js
@@ -0,0 +1,16 @@
+//burger
+let burgerButton = document.getElementById('toggleButton');
+let navBar = document.getElementById('navbarlinks');
+let firstLine = document.getElementById('burger1');
+let secondline = document.getElementById('burger2');
+let thirdline = document.getElementById('burger3')
+
+
+burgerButton.addEventListener('click', function() {
+    navBar.classList.toggle('activeNav');
+    firstLine.classList.toggle('active');
+    secondline.classList.toggle('active');
+    thirdline.classList.toggle('active');
+
+})
+
